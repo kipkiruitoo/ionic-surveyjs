de54ddcbdb93867a0b471e5fe0b6b308ac52c5c8
gkarumba <gachegua@gmail.com>
2020-05-05 16:04:32 +0300

    Add the wallet page

---
+++ src/app/wallet/wallet/wallet.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { WalletComponent } from './wallet.component';
+
+describe('WalletComponent', () => {
+  let component: WalletComponent;
+  let fixture: ComponentFixture<WalletComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WalletComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WalletComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/wallet/wallet/wallet.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet',
+  templateUrl: './wallet.component.html',
+  styleUrls: ['./wallet.component.scss'],
+})
+export class WalletComponent implements OnInit {
+  logs = [
+    {time: new Date(), title: 'Withdraw', amount: '100', icon: 'remove_circle_outline'},
+  // tslint:disable-next-line: max-line-length
+  {time: new Date(), title: 'Payment',  amount: '100', icon: 'add_circle_outline'},
+  // tslint:disable-next-line: max-line-length
+  {time: new Date(), title: 'Withdraw',  amount: '200', icon: 'remove_circle_outline'},
+  // tslint:disable-next-line: max-line-length
+  {time: new Date(), title: 'Withdraw',  amount: '300', icon: 'remove_circle_outline'},
+  // tslint:disable-next-line: max-line-length
+  {time: new Date(), title: 'Withdraw', amount: '400', icon: 'remove_circle_outline'},
+  // tslint:disable-next-line: max-line-length
+  {time: new Date(), title: 'Payment',  amount: '600', icon: 'add_circle_outline'} ];
+  constructor() { }
+
+  ngOnInit() {}
+
+}
